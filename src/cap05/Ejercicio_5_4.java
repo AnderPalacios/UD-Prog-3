59f37d3c1758b9363808e7b5e400debcc68d9933
apala <apala@10.166.59.247>
2023-11-07 13:39:53 +0100

    Ejercicios

--- src/cap05/Ejercicio_5_4.java
+++ src/cap05/Ejercicio_5_4.java
@@ -1,5 +1,7 @@
 package cap05;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Ejercicio_5_4 {
@@ -14,7 +16,19 @@ public class Ejercicio_5_4 {
 		HashMap<String, String> mapaMinsmaEntidad2 = new HashMap<String, String>();
 		
 		
-		//Mapa de valores múltiples (SEGUIR AQUÍ, TERCER APARTADO (los dos mas interesantes))
+		//Mapa de valores múltiples:
+		HashMap<Integer, ArrayList<String>> mapaValoresMultiples = new HashMap<Integer, ArrayList<String>>();
+		
+		//Mapa de mapas
+		HashMap<Integer, HashMap<String, Integer>> mapaDeMapas = new HashMap<Integer, HashMap<String,Integer>>();
+		
+		//Para practicar:
+		HashMap<String, Integer> ints = new HashMap<String, Integer>();
+		ints.put("Ander", 14);
+		mapaDeMapas.put(1, ints);
+		Ejercicio_5_4 ej = new Ejercicio_5_4();
+		ej.recorrerMapa4(mapaDeMapas);
+		
 	}
 	
 	//Métodos para mapa de entidad:
@@ -69,5 +83,84 @@ public class Ejercicio_5_4 {
 	}
 	
 	
+	//Métodos para mapa de valores múltiples:
 	
+	//Método de creación:
+	private HashMap<Integer, ArrayList<String>> crearMapa3() {
+		return new HashMap<Integer, ArrayList<String>>();
+	}
+	
+	//Método de carga:
+	private void cargarMapa3(HashMap<Integer, ArrayList<String>> mapaACargar, int k, ArrayList<String> arrayACargar) {
+		if (!mapaACargar.containsKey(k)) {
+			mapaACargar.put(k, arrayACargar);
+		} else {
+			ArrayList<String> yaEstaClave = mapaACargar.get(k);
+			for (String s: arrayACargar) {
+				yaEstaClave.add(s);
+			}
+			mapaACargar.put(k, yaEstaClave);
+		}
+	}
+	
+	//Método consulta Mapa:
+	private ArrayList<String> consultaMapa2(HashMap<Integer, ArrayList<String>> mapaCons, int k) {
+		if (!mapaCons.containsKey(k)) {
+			return mapaCons.get(k);
+		}
+		return null;
+	}
+	
+	
+	//Método para recorrer el mapa:
+	private void recorrerMapa3(HashMap<Integer, ArrayList<String>> mapaRec) {
+		for (Integer key: mapaRec.keySet()) {
+			System.out.println(key);
+			for (String s: mapaRec.get(key)) {
+				System.out.println(s);
+			}
+		}
+	}
+	
+	
+	//Métodos para mapa de mapas:
+	
+	//Método de creación:
+	private HashMap<Integer, HashMap<String, Integer>> crearMapa4() {
+		return new HashMap<Integer, HashMap<String,Integer>>();
+	}
+	
+	//Método de carga:
+	private void cargarMapa4(HashMap<Integer, HashMap<String, Integer>> mapaMapa, int key, HashMap<String, Integer> map, String s)  {
+		if (!mapaMapa.containsKey(key)) {
+			mapaMapa.put(key, map);
+		} else { // Si la clave principal ya existe, actualizamos el mapa secundario con la clave secundaria y su valor.
+			HashMap<String, Integer> mapaSec = mapaMapa.get(key);
+			mapaSec.put(s, mapaSec.get(s));
+			
+		}
+	}
+	
+	//Método consulta mapa:
+	private HashMap<String, Integer> consultaMapa4(HashMap<Integer, HashMap<String, Integer>> mapaC, int clave) {
+		if (mapaC.containsKey(clave)) {
+			return mapaC.get(clave);
+		}
+		return null;
+	}
+	
+	private void recorrerMapa4(HashMap<Integer, HashMap<String, Integer>> mapaRec) {
+		for (Integer k: mapaRec.keySet()) {
+			System.out.println(mapaRec.get(k));
+			for (String s: mapaRec.get(k).keySet()) {
+				System.out.println(mapaRec.get(k).get(s));
+			}
+			/*
+			 * HashMap<String, Integer> m = mapaRec.get(k);
+			 * for (String s: m.keySet(){
+			 * 	syso(m.get(s))
+			 * {
+			 */
+		}
+	}
 }
